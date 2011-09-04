a9e240aa5f9fe067126b6456ee4f9bb940827ade
aip <shavrin.ip@gmail.com>
2011-09-04 19:03:58 +0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aitest::Application.config.secret_token = '1f7c284b77782220aa9f7b9348d86e5c01db9a7001a677e6065bf773f877139d0ab6ec7fdf5f6854b3c097ef58ee7bce97f6e72f78852b22e82a75d7696227d8'
